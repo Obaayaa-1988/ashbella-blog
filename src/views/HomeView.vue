fb662563abad6d35c070596d69cdfaacd2b70644
Abigail <ashbella333@gmail.com>
2024-01-10 16:03:00 +0000

    init

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,18 +1,42 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <p>My name is: {{ name }} and I am {{ age }} years old</p>
+  <button @click="buttonClicked">Click me</button>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
 
 export default {
   name: 'HomeView',
-  components: {
-    HelloWorld
+  //composition api// this function runs before any lifecycle hooks
+ setup(){
+  console.log('setup')
+  let name = 'Eno Mary';
+  let age = 2;
+
+  //writing arrow functions
+  const buttonClicked = () => {
+    console.log('you clicked me')
   }
+
+  return {name: name, age: age, buttonClicked}// not reactive values
+
+ },
+
+//  data(){
+//   return{
+//     career: 'doctor'// reactive values
+//   }
+//  }
+//  created(){
+//   console.log('created')
+
+//  },
+//  mounted(){
+//   console.log('mounted')
+
+//  },
 }
 </script>
